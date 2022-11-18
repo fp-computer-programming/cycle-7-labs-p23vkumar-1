01d8d66693b80871263108d8ae5c766ace3b7f43
Vishnu Kumar <p23vkumar@fairfieldprep.org>
2022-11-18 11:17:34 -0500

    Lab 7-1
    Lab 7-1

---
+++ Lab 7-1.py
@@ -0,0 +1,12 @@
+#1. Create a Python file named lab_7-1.py
+#2. Create a function called greeting()
+def greeting():
+#3. Add a docstring to the function that explains how the function can only print "Hello World" on one line
+    '''print: Hello World'''
+#4. Add a statement in the function to print "Hello World!"
+    x = "Hello World"
+#5. Add a statement that returns the docstring for the greeting
+#function
+    return (x)
+#6. Add another statement to call the function
+print (greeting())
