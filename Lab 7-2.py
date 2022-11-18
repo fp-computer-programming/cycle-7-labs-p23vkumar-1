a7234f510da7188540fc6dbe4a561fe9d515d451
Vishnu Kumar <p23vkumar@fairfieldprep.org>
2022-11-18 11:35:12 -0500

    Lab 7-2
    Lab 7-2

---
+++ Lab 7-2.py
@@ -0,0 +1,18 @@
+#1. Create a Python file named lab_7-2.py
+#2. Create a function called find_sum()
+#3. Add the parameters num1 and num2 to the definition of find_sum()
+def find_sum(num1, num2):
+#4. Add statements to the function that adds the arguments passed to num1 and num2 and 
+#stores them in a new variable num_sum
+    num_sum = num1 + num2
+    return num_sum #Finish the function with a statement that returns num_sum
+
+#6. Create a statement that calls the function to find the sum of 111 and 222. Set it
+#equal to the variable my_sum
+my_sum = find_sum(111, 222)
+#7. Add another print statement after the previous statement that prints my_sum
+print(my_sum)
+
+#8. What happens when you run the program?
+
+print("After I run the program, the value of 333 is printed")
